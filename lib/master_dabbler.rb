74f5f2146b60822f69de35287308de064ffb4c84
Adam <atkolkma@gmail.com>
2013-12-19 17:00:23 -0500

    merges carfax data with used inventory data

--- lib/master_dabbler.rb
+++ lib/master_dabbler.rb
@@ -15,7 +15,7 @@ inventory = aul_dabbler.create_vehicle_table
 
 ######Grabs vehicle feature data from AUL on all used vehicles with no comments########
 
-# used_inventory_data = inventory.used.no_comments
+used_inventory_data = inventory.used.no_comments
 
 # used_inventory_data.each do |n|
 # 	n["features"] = aul_dabbler.grab_vehicle_features(n["aul_id"])["features"]
@@ -31,4 +31,17 @@ carfax_dabbler = CarFaxDabbler.new
 carfax_dabbler.carfax_login
 
 puts carfax_dabbler.grab_report_details("1G1PC5SH4C7269712")
+
+used_inventory_data.each do |n|
+	vehicle_data = carfax_dabbler.grab_report_details(n["vin"])
+	n["body_type"] = vehicle_data["body_type"]
+	n["engine"] = vehicle_data["engine"]
+	n["drive_train"] = vehicle_data["drive_train"]
+	n["full_title"] = vehicle_data["full_title"]
+	n["damage"] = vehicle_data["damage"]
+	n["certified"] = vehicle_data["certified"]
+	
+end
+
+puts used_inventory_data
 
