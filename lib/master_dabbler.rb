e93b71102dbc463b2139fcd6ac6335d5738f3219
Adam <atkolkma@gmail.com>
2013-12-20 10:39:22 -0500

    grab more data from CARFAX

--- lib/master_dabbler.rb
+++ lib/master_dabbler.rb
@@ -17,21 +17,19 @@ inventory = aul_dabbler.create_vehicle_table
 
 used_inventory_data = inventory.used.no_comments
 
-used_inventory_data.each do |n|
-	vehicle_data = aul_dabbler.grab_vehicle_features(n["aul_id"])
-	n["features"] = vehicle_data["features"]
-	n["engine"] = vehicle_data["engine"]
-	n["trans"] = vehicle_data["trans"]
-	n["miles"] = vehicle_data["miles"]
-	n["color"] = vehicle_data["color"]
-end
+# used_inventory_data.each do |n|
+# 	vehicle_data = aul_dabbler.grab_vehicle_features(n["aul_id"])
+# 	n["features"] = vehicle_data["features"]
+# 	n["engine"] = vehicle_data["engine"]
+# 	n["trans"] = vehicle_data["trans"]
+# 	n["miles"] = vehicle_data["miles"]
+# 	n["color"] = vehicle_data["color"]
+# end
 
 #####Login to Carfax and add vehicle data to used_inventory_data
 
 carfax_dabbler = CarFaxDabbler.new
 carfax_dabbler.carfax_login
-
-puts carfax_dabbler.grab_report_details("1G1PC5SH4C7269712")
 
 used_inventory_data.each do |n|
 	vehicle_data = carfax_dabbler.grab_report_details(n["vin"])
@@ -41,7 +39,8 @@ used_inventory_data.each do |n|
 	n["full_title"] = vehicle_data["full_title"]
 	n["damage"] = vehicle_data["damage"]
 	n["certified"] = vehicle_data["certified"]
-	
+	n["maintenance"] = vehicle_data["maintenance"]
+	n["number_owners"] = vehicle_data["number_owners"]
 end
 
 puts used_inventory_data
